243b5acefc88f922ac92fd053978d85b051bd721
Adarsh Ashesh <adarshashesh@gmail.com>
2022-05-23 00:32:17 +0530

    first commit

---
+++ db/config.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+//mongoose.connect("mongodb+srv://username:12345*@cluster0.nmpry.mongodb.net/e-comm?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://user:12345*@cluster0.x6phh.mongodb.net/Database1?retryWrites=true&w=majority")
